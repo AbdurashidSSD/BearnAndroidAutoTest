60a1f67a291b03633e7ddd981dce67e6fef4b735
Abdurashid <ajoraboyev@bearncorp.com>
2023-11-02 12:01:06 +0500

    Added Sign Up page

--- src/main/java/org/bearn/TestCases.java
+++ src/main/java/org/bearn/TestCases.java
@@ -3,6 +3,7 @@ package org.bearn;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.bearn.pages.SignInPage;
+import org.bearn.pages.SignUpPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,13 +12,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import com.github.javafaker.Faker;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class SignInPageTest {
+public class TestCases {
     private AndroidDriver<AndroidElement> driver;
     private SignInPage signInPage;
+    private SignUpPage signUpPage;
+    private Faker faker;
 
     @BeforeMethod
     public void setup() throws MalformedURLException {
@@ -30,9 +34,11 @@ public class SignInPageTest {
         URL appiumServerURL = new URL("http://localhost:4723/wd/hub/");
         driver = new AndroidDriver<>(appiumServerURL, desiredCapabilities);
         signInPage = new SignInPage(driver);
+        signUpPage = new SignUpPage(driver);
+        faker = new Faker();
     }
     @Test(priority = 0)
-    public void loginTestPositive() throws InterruptedException {
+    public void signInTestPositive() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.bearn.app.snapshot:id/login_button_signin")));
         signInPage.enterUsername("abdurj7777@gmail.com");
@@ -41,13 +47,30 @@ public class SignInPageTest {
         Thread.sleep(10000);
     }
     @Test(priority = 1)
-    public void loginTestNegative() throws InterruptedException {
+    public void signInTestNegative() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.bearn.app.snapshot:id/login_button_signin")));
         signInPage.enterUsername("abdurj7777@gmail.com");
         signInPage.enterPassword("123");
         signInPage.clickLoginButton();
         Thread.sleep(10000);
+    }
+    @Test
+    public void signUpTestPositive(){
+        String email = faker.internet().emailAddress();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.bearn.app.snapshot:id/login_button_signin")));
+        signUpPage.clickSignUpButton();
+        WebElement label = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.bearn.app.snapshot:id/label")));
+        signUpPage.enterAccessCode("579a1a");
+        signUpPage.clickContinueButtonAccess();
+        WebElement label1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.bearn.app.snapshot:id/sign_up_edit_email_et")));
+        signUpPage.enterFirstName(faker.name().name());
+        signUpPage.enterLastName(faker.name().lastName());
+        signUpPage.enterEmail(email);
+        signUpPage.enterConfirmEmail(email);
+        signUpPage.clickCheckbox();
+        signUpPage.pressSignUp();
     }
     @AfterMethod
     public void teardown() {
--- src/main/java/org/bearn/pages/SignUpPage.java
+++ src/main/java/org/bearn/pages/SignUpPage.java
@@ -10,14 +10,60 @@ public class SignUpPage {
     private WebDriver driver;
 
     // Page Elements
-    @AndroidFindBy(id = "")
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/login_button_signup")
+    private MobileElement signUpButton;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ViewSwitcher/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.EditText")
+    private MobileElement accessCodeField;
+
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/sign_up_button")
+    private MobileElement continueButtonAccess;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ViewSwitcher/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.EditText")
+    private MobileElement firstNameField;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ViewSwitcher/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.EditText")
+    private MobileElement lastNameField;
+
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/sign_up_edit_email_et")
     private MobileElement emailField;
 
-    @AndroidFindBy(xpath = "")
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/sign_up_edit_email_confirm_et")
+    private MobileElement confirmEmailField;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ViewSwitcher/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[5]/android.widget.FrameLayout/android.widget.EditText")
     private MobileElement passwordField;
 
-    @AndroidFindBy(id = "")
-    private MobileElement signInButton;
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/sign_up_edit_password_confirm_et")
+    private MobileElement confirmPasswordField;
+
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/sign_up_t_c_checkbox")
+    private MobileElement privacyPolicyCheckbox;
+
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/sign_up_button")
+    private MobileElement signUpButtonRegistration;
+
+    @AndroidFindBy(id = "com.bearn.app.snapshot:id/button3")
+    private MobileElement continueGettingStartedButton;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ViewSwitcher/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.ToggleButton")
+    private MobileElement connectGoogleFit;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ViewSwitcher/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.ToggleButton")
+    private MobileElement connectFitbit;
+
+    @AndroidFindBy(id = "com.android.chrome:id/terms_accept")
+    private MobileElement acceptChromeTerms;
+
+    @AndroidFindBy(id = "ember591")
+    private MobileElement accountEmailFitbitField;
+
+    @AndroidFindBy(id = "ember592")
+    private MobileElement passwordAccountFitbit;
+
+    @AndroidFindBy(id = "ember632")
+    private MobileElement signInToFitbit;
+
 
     // Constructor
     public SignUpPage(WebDriver driver) {
@@ -26,15 +72,37 @@ public class SignUpPage {
     }
 
     // Page Methods
-    public void enterUsername(String username) {
-        emailField.sendKeys(username);
+    public void clickSignUpButton() {
+        signUpButton.click();
     }
-
-    public void enterPassword(String password) {
-        passwordField.sendKeys(password);
+    public void enterAccessCode(String code) {
+        accessCodeField.sendKeys(code);
     }
-
-    public void clickLoginButton() {
-        signInButton.click();
+    public void clickContinueButtonAccess() {
+        continueButtonAccess.click();
+    }
+    public void enterFirstName(String name){
+        firstNameField.sendKeys(name);
+    }
+    public void enterLastName(String lastname){
+        lastNameField.sendKeys(lastname);
+    }
+    public void enterEmail(String email){
+        emailField.sendKeys(email);
+    }
+    public void enterConfirmEmail(String confirmEmail){
+        confirmEmailField.sendKeys(confirmEmail);
+    }
+    public void enterPassword(String pass){
+        passwordField.sendKeys(pass);
+    }
+    public void confirmPassword(String confirmPass){
+        confirmPasswordField.sendKeys(confirmPass);
+    }
+    public void clickCheckbox(){
+        privacyPolicyCheckbox.click();
+    }
+    public void pressSignUp(){
+        signUpButton.click();
     }
 }
